2e6216f1030ef481ab419230d2730f8e1f7576ac
jason.yf <jason.yf@alibaba-inc.com>
2021-06-28 15:57:27 +0800

    feat: update

--- src/executeTransforms.js
+++ src/executeTransforms.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const execa = require('execa');
-const config = require('../transforms/config.json');
 const { getProjectType, getProjectFramework, getProjectLanguageType } = require('@appworks/project-utils');
+const config = require('../transforms/config.json');
 
 const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
 
-async function executeTransforms(cwd, files, transforms, mode) {
+async function executeTransforms(cwd, files, transforms, mode, jscodeshiftOptions) {
   // Add project info to transform option
   const transformOptions = [
     `--projectType=${await getProjectType(cwd, true)}`,
@@ -14,40 +14,52 @@ async function executeTransforms(cwd, files, transforms, mode) {
   ];
 
   const workers = transforms.map((transform) => {
-    return async () => {
+    return new Promise((resolve) => {
+      let output = '';
       let args = mode === 'check' ? ['--dry'] : [];
 
       args = args.concat(['--transform', transform]);
       args = args.concat(files);
+      args = args.concat(jscodeshiftOptions || []);
       args = args.concat(transformOptions);
-
-      const { stdout } = await execa(jscodeshiftExecutable, args);
-
-      // Remove all colors/styles from strings https://stackoverflow.com/questions/25245716/remove-all-ansi-colors-styles-from-strings
-      const output = stdout.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
 
       const transformName = path.basename(transform, path.extname(transform));
       const transformConfig = config[transformName] || {};
-      if (
-        mode === 'run' ||
-        // check mode return can run codemods when
-        // 1. jscodeshift show not 0 ok if matched
-        // 2. codemod which severity >= 1
-        (!/\n0 ok\n/.test(output) && transformConfig.severity >= 1)
-      ) {
-        return {
-          ...transformConfig,
-          transform: transformName,
-          docs: `https://github.com/appworks-lab/codemod/tree/master/transforms/docs/${transformName}.md`,
-          mode,
-          output,
-        };
-      }
-      return null;
-    };
+
+      const childProcess = execa(jscodeshiftExecutable, args);
+
+      console.log(`Transform '${transformName}' start.`);
+
+      childProcess.stdout.pipe(process.stdout);
+      childProcess.stdout.on('data', (data) => {
+        output += data.toString();
+      });
+
+      childProcess.on('exit', () => {
+        // Remove all colors/styles from strings https://stackoverflow.com/questions/25245716/remove-all-ansi-colors-styles-from-strings
+        output = output.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
+
+        if (
+          mode === 'run' ||
+          // check mode return can run codemods when
+          // 1. jscodeshift show not 0 ok if matched
+          // 2. codemod which severity >= 1
+          (!/\n0 ok\n/.test(output) && transformConfig.severity >= 1)
+        ) {
+          resolve({
+            ...transformConfig,
+            transform: transformName,
+            docs: `https://github.com/appworks-lab/codemod/tree/master/transforms/docs/${transformName}.md`,
+            mode,
+            output,
+          });
+        }
+        resolve(null);
+      });
+    });
   });
 
-  const results = await Promise.all(workers.map((work) => work()));
+  const results = await Promise.all(workers);
   return results.filter((result) => result);
 }
 
